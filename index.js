807753a3017ecf9bb1b31423b59472a4f8dadd48
timothyylim <timothylim23@gmail.com>
2018-02-01 15:18:26 +0100

    HFP-1839 Create semantics

---
+++ index.js
@@ -0,0 +1,30 @@
+var H5P = H5P || {};
+
+H5P.IVOpenEndedQuestion = (function ($) {
+  /**
+   * Constructor function.
+   */
+  function C(options, id) {
+    this.$ = $(this);
+    // Extend defaults with provided options
+    this.options = $.extend(true, {}, {
+      greeting: 'Hello world!',
+      image: null
+    }, options);
+    // Keep provided id.
+    this.id = id;
+  };
+
+  /**
+   * Attach function called by H5P framework to insert H5P content into
+   * page
+   *
+   * @param {jQuery} $container
+   */
+  C.prototype.attach = function ($container) {
+    var self = this;
+    $container.append('<div class="greeting-text">' + this.options.greeting + '</div>');
+  };
+
+  return C;
+})(H5P.jQuery);
